31f8b3302415240e74d17af518998082b3aeb1ef
Zeeshan Safdar <zeeshan.safdar@starzplayarabia.com>
2024-12-31 16:26:24 +0500

    add row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 // it (styled.) returns us a React element, which we will use in our render logic
 const StyledApp = styled.div`
-  background-color: yellow;
-  &:hover {
-    background-color: green;
-  }
+  padding: 20px;
 `;
 
 function App() {
@@ -16,9 +16,24 @@ function App() {
       {/* GlobalStyles needs to be sibling, it does not accept any children */}
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Description</Heading>
-        <Heading as="h3">Forms</Heading>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button>Check out</Button>
+            </div>
+          </Row>
+
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="number of guests" />
+              <Input type="number" placeholder="number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
