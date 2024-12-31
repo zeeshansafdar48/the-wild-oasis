30785a6fab2285e3983dc77565a116d521e957f4
Zeeshan Safdar <zeeshan.safdar@starzplayarabia.com>
2024-12-31 11:59:39 +0500

    install styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 32px;
+  font-weight: 900;
+`;
+
+const Input = styled.input`
+  padding: 12px 20px;
+  border: 1px solid #ddd;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <H1>Testing</H1>
+      <Input type="number" placeholder="Please add number" />
+    </div>
+  );
 }
 
 export default App;
