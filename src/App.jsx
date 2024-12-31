4678cf39e8344daabc802abce132fa4f6c705f68
Zeeshan Safdar <zeeshan.safdar@starzplayarabia.com>
2024-12-31 15:18:55 +0500

    global styles + css function + props in styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 32px;
-  font-weight: 900;
-`;
-
-const Input = styled.input`
-  padding: 12px 20px;
-  border: 1px solid #ddd;
+// it (styled.) returns us a React element, which we will use in our render logic
+const StyledApp = styled.div`
+  background-color: yellow;
+  &:hover {
+    background-color: green;
+  }
 `;
 
 function App() {
   return (
-    <div>
-      <H1>Testing</H1>
-      <Input type="number" placeholder="Please add number" />
-    </div>
+    <>
+      {/* GlobalStyles needs to be sibling, it does not accept any children */}
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Description</Heading>
+        <Heading as="h3">Forms</Heading>
+      </StyledApp>
+    </>
   );
 }
 
