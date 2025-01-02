16e3465d6dc19acb30508e10bef3d66732c16c2c
Zeeshan Safdar <zeeshan.safdar@starzplayarabia.com>
2025-01-02 15:23:41 +0500

    add supabase in the project

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.log("Cabins could not be loaded");
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
